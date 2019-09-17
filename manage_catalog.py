d6a552b798c11dbe577cb500d0e49e870b9a7c87
Matheus J. Castro <matheusj_castro@icloud.com>
2019-09-17 18:33:12 -0300

    Add plot and finalize manage

--- manage_catalog.py
+++ manage_catalog.py
@@ -1,49 +1,112 @@
 ###########################################
-# Display .FITS file                      #
+# Manage Fits Catalog                     #
 # Matheus J. Castro                       #
-# Version 2.0                             #
-# Last Modification: 09/09/2019           #
+# Version 1.0                             #
+# Last Modification: 17/09/2019           #
 ###########################################
 
-# learn more at: http://learn.astropy.org/FITS-images.html
-# example for ZScale: https://qiita.com/phyblas/items/87667c250b29e195f7fb
-
 import numpy as np
-import matplotlib.pyplot as plt
 from astropy.io import fits
 
-catalog = 'j02-20170711T001143-01_proc.proccat'  # name for your fits file
-catalog_file = fits.open(catalog)
+######################################################################################
 
-info = catalog_file.info(0)
-size = len(info)
 
-#print(catalog_file.info())
+def save_catalog(catalog_file, ext_names, str_element):
+    data = get_data(catalog_file)  # Get the data
+    col = len(data[0])  # length of columns
+    line = len(data)  # length of lines
 
-ext_names = []
-for i in range(size):
-    ext_names.append(info[i][1])
+    # create a list new_data of all the data and reformulating the ndarrays found inside data array
+    # useful for save in csv file
+    new_data = []
+    for i in range(line):
+        for j in range(col):
+            if type(data[i][j]) == np.ndarray:
+                array = ""
+                for k in range(len(data[i][j])):
+                    array = array + " " + repr(data[i][j][k])
+                array = array[1:]
+                new_data.append(array)
+            else:
+                new_data.append(data[i][j])
+    new_data = np.asarray(new_data).reshape(line, col)
 
-#for i in range(1, size-1):
+    np.savetxt("Extension{}_{}.csv".format(2, ext_names[2]), new_data, header=str_element, fmt="%s", delimiter=",")
 
-header = np.array(repr(catalog_file[2].header))
-np.savetxt("Header.txt", [header], fmt="%s")
+######################################################################################
 
-dic = np.array(catalog_file[2].header)
-header = catalog_file[2].header
-element = []
-for i in range(len(dic)):
-    if "TTYPE" in dic[i]:
-        element.append(header[i])
-#print([element])
 
-np.savetxt("Data.txt", [element], fmt="%s", delimiter=",")
+def get_header(catalog_file, save=False, extension=2):
+    if save:
+        # save the header of extension of interest
+        header = np.array(repr(catalog_file[extension].header))
+        np.savetxt("Header.txt", [header], fmt="%s")
 
-l = len(catalog_file[2].data[0])
-data = catalog_file[2].data[0]
-print(data)
+    # create the elemente list with names of the collumns of the catalog
+    dic = np.array(catalog_file[extension].header)
+    header = catalog_file[extension].header
+    element = []
+    for i in range(len(dic)):
+        if "TTYPE" in dic[i]:
+            element.append(header[i])
 
-#data = np.array(repr(catalog_file[2].data))
-np.savetxt("Extension{}_{}.csv".format(2, ext_names[2]), data, fmt="%s", delimiter=",")
+    # np.savetxt("Data.txt", [element], fmt="%s", delimiter=",")
 
-catalog_file.close()
+    # transfor element array into a string of the elements
+    str_element = ""
+    for i in range(len(element)):
+        str_element = str_element + "," + repr(element[i])
+    str_element = str_element[1:].replace("'","")
+    # print(str_element)
+
+    return element, str_element
+
+######################################################################################
+
+
+def get_data(catalog_file, extension=2):
+    # Save the data
+    data = catalog_file[extension].data
+
+    return data
+
+######################################################################################
+
+
+def get_info(catalog_file):
+    # Get information about names of extension of the .proccat
+    info = catalog_file.info(0)
+    size = len(info)
+
+    # print(catalog_file.info())
+
+    # Create a list of the names of the extensions
+    ext_names = []
+    for i in range(size):
+        ext_names.append(info[i][1])
+
+    info = catalog_file.info()
+
+    return info, ext_names
+
+######################################################################################
+
+
+def cat_open(cat_name):
+
+    # Open the catalog
+    catalog_file = fits.open(cat_name)
+
+    return catalog_file
+
+######################################################################################
+
+
+def close(catalog_file):
+    catalog_file.close()
+
+######################################################################################
+
+
+if __name__ == '__main__':
+    cat_open()
---
+++ plot_catalog.py
@@ -0,0 +1,45 @@
+###########################################
+# Plot Fits Catalog                       #
+# Matheus J. Castro                       #
+# Version 1.0                             #
+# Last Modification: 17/09/2019           #
+###########################################
+
+import numpy as np
+import matplotlib.pyplot as plt
+import manage_catalog as mancat
+
+catalog = mancat.cat_open('j02-20170711T001143-01_proc.proccat')
+
+# save catalog in a csv file
+# mancat.save_catalog(catalog, mancat.get_info(catalog)[1], mancat.get_header(catalog)[1])
+
+# get info about catalog
+# check the extension do you want to use, the default is 2.
+# info = mancat.get_info(catalog)[0]
+
+elements = mancat.get_header(catalog)[0]  # elements of catalog
+data = mancat.get_data(catalog)  # data of catalog
+mancat.close(catalog)
+
+print(elements)
+
+x_axis = "X_IMAGE"
+y_axis = "Y_IMAGE"
+
+x_column = elements.index(x_axis)
+y_column = elements.index(y_axis)
+
+x_points = []
+y_points = []
+for i in range(len(data)):
+    x_points.append(data[i][x_column])
+    y_points.append(data[i][y_column])
+
+plt.figure(figsize=(8, 6))
+plt.xlabel(x_axis)
+plt.ylabel(y_axis)
+plt.title("{} and {}".format(x_axis, y_axis))
+plt.plot(x_points, y_points, ".")
+
+plt.show()
